91905d3289d23005e5eb4ca4e3603a4044ec2393
Mahipat <mj@mojaave.com>
2020-04-30 02:01:29 +0530

    Added site meta info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,12 @@
+const siteMetadata = require('./data/site-config')
+
 module.exports = {
   plugins: [
       {
           resolve: 'gatsby-theme-factly',
           options: {
             client: 'factly',
+            metaData: siteMetadata
           }
       }
   ]
